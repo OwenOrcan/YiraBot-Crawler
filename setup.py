ef1f9a393efe289bca7ef15181b401d311242b6c
OwenOrcan <owenorcan@gmail.com>
2024-01-26 01:57:43 -0500

    YiraBot 1.0.7.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='YiraBot',
-    version='1.0.7.1',
+    version='1.0.7.2',
     packages=find_packages(),
     install_requires=open('requirements.txt').readlines(),
     author='Owen Orcan',
