2a9d3bc2a88df1705d4172e1232b9737581983b0
OwenOrcan <owenorcan@gmail.com>
2024-02-11 18:35:24 -0500

    Finished Crawling Class

--- yirabot/tests/test2.py
+++ yirabot/tests/test2.py
@@ -2,13 +2,6 @@ from yirabot import Crawler
 
 bot = Crawler()
 
-routes = bot.validate_routes("https://yira.me/static/sitemap.xml")
+data = bot.validate_routes("https://yira.me/static/sitemap.xml")
 
-data = bot.crawl("https://yira.me")
-
-content = bot.scrape("https://yira.me")
-
-print("Routes: ",routes)
-print("Data: ", data)
-print("Content: ",content)
-
+print(data)
