134d322e935d448e046ea358cd07c16895896c64
OwenOrcan <owenorcan@gmail.com>
2024-03-02 20:26:44 -0500

    1.0.9.1

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='YiraBot',
-    version='1.0.9.1',
+    version='1.0.9.2',
     packages=find_packages(),
     install_requires=open('requirements.txt').readlines(),
     author='Owen Orcan',
     author_email='owenorcan@gmail.com',
     url='https://github.com/OwenOrcan/Yirabot-Crawler',
-    license='MIT License',
+    license='GNU General Public License (Version 3)',
     description="YiraBot: Simplifying Web Scraping for All. A user-friendly tool for developers and enthusiasts, offering command-line ease and Python integration. Ideal for research, SEO, and data collection.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
